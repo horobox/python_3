db256caf873ff3274aba3941e31289854f2ee110
horobox <horometer@protonmail.com>
2019-09-30 09:51:56 +0530

    Added 30-09-2019 programs

---
+++ Chapter1/Examples/lowUp.py
@@ -0,0 +1,3 @@
+name = "Shree vathsa"
+print(name.upper())
+print(name.lower())
---
+++ Chapter1/Examples/name.py
@@ -0,0 +1,6 @@
+name = "shree vathsa"
+name1 = "SHREE VATHSA"
+name2 = "sHREE vATHSA"
+print(name.title())
+print(name1.title())
+print(name2.title())
