6868568426c851ea32715d7382499d5c95fda132
horobox <horometer@protonmail.com>
2019-10-07 22:56:03 +0530

    Added 07-10-2019 programs

---
+++ Chapter3/Problems/3_4.py
@@ -0,0 +1,20 @@
+my_favourite_places = ['Tokyo', 'Switzerland', 'California', 'Mexico']
+print(my_favourite_places)
+
+print(sorted(my_favourite_places))
+print(my_favourite_places)
+
+print(sorted(my_favourite_places, reverse=True)) #reversing order using sorted
+print(my_favourite_places)
+
+my_favourite_places.reverse()
+print(my_favourite_places)
+
+my_favourite_places.reverse()
+print(my_favourite_places)
+
+my_favourite_places.sort()
+print(my_favourite_places)
+
+my_favourite_places.sort(reverse=True)
+print(my_favourite_places)
