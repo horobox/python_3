71e0b5bb96a72a358dde717ced8683a6cf0bc033
horobox <54677025+horobox@users.noreply.github.com>
2019-10-08 18:34:20 +0530

    Delete 2_3.py

--- Chapter1/Problems/2_3.py
+++
@@ -1,8 +0,0 @@
-first_name = "shree"
-second_name = "vathsa"
-
-full_name = first_name + " " + second_name
-
-message = "Hello " + full_name.title() + ", would you like to learn some python today?"
-
-print(message)
