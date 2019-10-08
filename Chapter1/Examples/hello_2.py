1dc57e8d31d4f15788626c0ee43019afb27bbdfa
horobox <54677025+horobox@users.noreply.github.com>
2019-10-08 18:33:48 +0530

    Delete hello_2.py

--- Chapter1/Examples/hello_2.py
+++
@@ -1,3 +0,0 @@
-print('I told my friend, "Python is my favorite language!"')
-print("The language 'Python' is named after Monty Python, not the snake.")
-print("One of Python's strengths is its diverse and supportive community.")
